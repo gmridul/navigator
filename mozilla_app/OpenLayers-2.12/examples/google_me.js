8672ffdcd0d96da14a0b02a99e88e7837dd194fa
Anand Soni <anand.92.soni@gmail.com>
2013-06-10 15:49:40 +0530

    Trying markers, not working

--- mozilla_app/OpenLayers-2.12/examples/google_me.js
+++ mozilla_app/OpenLayers-2.12/examples/google_me.js
@@ -1,14 +1,21 @@
 var map;
 
+
 function init() {
     map = new OpenLayers.Map({
         div: "map",
         projection: new OpenLayers.Projection("EPSG:900913")
     });
-    
+	var markers = new OpenLayers.Layer.Markers( "Markers" );	
     var gmap = new OpenLayers.Layer.Google("Google Streets");
     
-    map.addLayers([gmap]);
+    map.addLayers([gmap,markers]);
+    var size = new OpenLayers.Size(21,25);
+    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+	var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+	markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(81.0,24.9),icon));
+	markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(81.0,24.9),icon.clone()));
+	
     //map.Size(150,150);
 
     //map.addControl(new OpenLayers.Control.LayerSwitcher());
@@ -21,3 +28,4 @@ function init() {
         5
     );
 }
+
